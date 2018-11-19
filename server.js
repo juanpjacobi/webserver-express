d86f6a3508b24e926b1ed6db6405f4a3cdeee4b0
Juan Pablo <juanpjacobi@gmail.com>
2018-11-19 00:38:36 -0300

    Primer commit

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+require('./hbs/helpers');
+const port = process.env.PORT || 3000;
+
+
+
+app.use(express.static(__dirname + '/public'));
+
+//Express hbs engine
+
+
+hbs.registerPartials(__dirname + '/views/parciales');
+app.set('view engine', 'hbs');
+
+
+app.get('/', (req, res) => {
+    // res.send('Hola mundo')
+
+
+    res.render('home', {
+        nombre: 'Fernando'
+
+    });
+});
+
+
+app.get('/about', (req, res) => {
+    // res.send('Hola mundo')
+
+
+    res.render('about');
+});
+
+
+app.listen(port, () => {
+    console.log(`Escuchando peticiones en el puerto ${port}`);
+})
